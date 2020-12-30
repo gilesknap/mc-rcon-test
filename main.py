d3d6f90e2fd9c619cebbc9ccc4868c956c3a10fb
Giles Knap <gilesknap@gmail.com>
2020-12-30 12:40:50 +0000

    got data command working

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from mcipc.rcon import Client
-from mcipc.rcon.types import FillMode, Vec3
+from mcipc.rcon.je import Client
+from mcipc.rcon.types import FillMode, TargetType, Vec3
 from time import sleep
 
-with Client("86.18.193.64", 25711, passwd="spiderMashTimewarp") as client:
-    start = Vec3(-14112, 99, 4100)
+
+with Client("localhost", 25701, passwd="spider") as client:
+    data = client.data.get(TargetType.ENTITY, "@p", "Pos")
+
+    print(f"data: {data}")
+
+    res = client.run(
+        "execute unless block -14116 98 4094 #minecraft:acacia_logs run say yes"
+    )
+    print(f"result {res}")
 
     print(client.players)
 
-    client.tell("@a", "Hello - testing testing")
+    # don't announce every rcon command
+    res = client.gamerule("sendCommandFeedback", False)
+    print(f"result {res}")
 
+
+    # send a message to ALL players
+    res = client.tell("@a", "Hello - testing testing")
+    print(f"result {res}")
+
+    # materialize a flashing wall
+    start = Vec3(-14112, 99, 4100)
     end = Vec3(int(start.x) + 10, int(start.y) + 10, start.z)
-    for i in range(10):
-        client.fill(start, end, "iron_block", FillMode.REPLACE)
+    for i in range(1):
+        res = client.fill(start, end, "iron_block", FillMode.REPLACE)
+        print(f"result {res}")
         sleep(1)
         client.fill(start, end, "air", FillMode.REPLACE)
         sleep(1)
+
+    res = client.run("say hello world")
+    print(f"result {res}")
 
