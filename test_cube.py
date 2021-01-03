0b897c2d8e58bd2b536540d50473587416e9d196
Giles Knap <gilesknap@gmail.com>
2021-01-03 17:36:11 +0000

    tidy cube

--- test_cube.py
+++ test_cube.py
@@ -1,8 +1,6 @@
 from mcipc.rcon.builder import Item, Vec3
 from mcipc.rcon.je import Client
-from numba import jit
 import numpy as np
-from time import sleep
 
 from helper import Helper
 
@@ -23,13 +21,19 @@ def make_cube(size: int):
 
     return [top] + [square] * (size - 2) + [bottom]
 
-#@jit(nopython=True)
+
+def render(client, mid, ncube, solid):
+    for idx, block in np.ndenumerate(ncube):
+        if solid[idx]:  # meh - why doesn't numpy do a filtered enumerate?
+            client.setblock(mid + Vec3(*idx), block)
+
+
 def spin(cube):
     with Client("localhost", 25901, passwd="spider") as client:
         mid = Vec3(-13, 5, 8)
         Helper(client).clear_blocks(mid, 60)
 
-        ncube = np.array(cube)
+        ncube = np.array(cube, Item)
         planes = [(0, 1), (0, 2), (1, 2)]
 
         solid = ncube != Item.AIR
@@ -37,11 +41,7 @@ def spin(cube):
         while True:
             for plane in planes:
                 for rot in range(9):
-                    sleep(0.5)
-                    for idx, block in np.ndenumerate(ncube):
-                        if solid[idx]: #  meh - why doesn't numpy do a filtered enumerate?
-                            client.setblock(mid + Vec3(*idx), block)
-
+                    render(client, mid, ncube, solid)
                     ncube = np.rot90(ncube, axes=plane)
 
 
