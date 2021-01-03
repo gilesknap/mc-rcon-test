431d5fb926f0dc5f6da92ae8937743df0286a2a4
Giles Knap <gilesknap@gmail.com>
2021-01-03 08:04:54 +0000

    hollow cube

--- test_cube.py
+++ test_cube.py
@@ -8,21 +8,15 @@ from numpy.lib.function_base import vectorize
 
 from helper import Helper
 
+left = [Item.RED_CONCRETE] * 10
+right = [Item.GREEN_CONCRETE] * 10
+row = [Item.BLUE_CONCRETE] + [Item.AIR] * 8 + [Item.YELLOW_CONCRETE]
+square = [left] + [row] * 8 + [right]
 
-p = [
-    [Item.RED_CONCRETE] * 10,
-    [Item.GREEN_CONCRETE] * 10,
-    [Item.BLUE_CONCRETE] * 10,
-    [Item.YELLOW_CONCRETE] * 10,
-    [Item.PINK_CONCRETE] * 10,
-    [Item.PURPLE_CONCRETE] * 10,
-    [Item.CYAN_CONCRETE] * 10,
-    [Item.GRAY_CONCRETE] * 10,
-    [Item.BLACK_CONCRETE] * 10,
-    [Item.WHITE_CONCRETE] * 10,
-]
+top = [[Item.WHITE_CONCRETE, Item.GRAY_CONCRETE] * 5] * 10
+bottom = [[Item.BLACK_CONCRETE, Item.GRAY_CONCRETE] * 5] * 10
 
-cube = [p] * 10
+cube = [top] + [square] * 8 + [bottom]
 
 
 def spin():
@@ -35,6 +29,7 @@ def spin():
 
         for plane in planes:
             for rot in range(9):
+                sleep(.2)
                 for idx, block in np.ndenumerate(ncube):
                     client.setblock(mid + Vec3(*idx), block)
 
